a9b7f3ffa377cdad9cbdb1496a2cb2faa183bf63
Andriy Kupchanko <80577186+andriykupchanko@users.noreply.github.com>
2024-02-17 22:19:09 +0200

    ad fire base

--- backend/main.js
+++ backend/main.js
@@ -1,15 +1,14 @@
 import { Telegraf, Markup } from "telegraf";
 
 const token = "6713815316:AAG4ekvQEVcdRq4Ii92dtKZ4xcwmvC9ID2I";
+const webUrl = "https://angular-tg-app-16382.web.app/";
 const bot = new Telegraf(token);
 
 bot.start(ctx => {
   ctx.reply("Hello!");
   ctx.replyWithHTML(
     "Click the button below:",
-    Markup.keyboard([
-      Markup.button.webApp("Open WebApp", "https://www.google.com")
-    ])
+    Markup.keyboard([Markup.button.webApp("Open WebApp", webUrl)])
   );
 });
 
--- frontend-angular-tg-app-mini/src/app/pages/shop/shop.component.ts
+++ frontend-angular-tg-app-mini/src/app/pages/shop/shop.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-shop',
   standalone: true,
   imports: [],
-  template: `<h1>fsdsfdfs</h1>`,
+  template: `<h1></h1>`,
 
 })
 export class ShopComponent {
