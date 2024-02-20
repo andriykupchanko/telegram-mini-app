0e51ce73cb51dbed8558ce5a2113567fdf1558a7
Andriy Kupchanko <80577186+andriykupchanko@users.noreply.github.com>
2024-02-20 23:04:52 +0200

    add product in project

--- frontend-angular-tg-app-mini/src/app/pages/shop/shop.component.ts
+++ frontend-angular-tg-app-mini/src/app/pages/shop/shop.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TelegramService } from '../../service/telegram.service';
 
 @Component({
   selector: 'app-shop',
   standalone: true,
   imports: [],
-  template: `<h1></h1>`,
+  template: `<h1>main page</h1>`,
 
 })
 export class ShopComponent {
-
+  telegram = inject(TelegramService);
+  constructor() {
+    this.telegram.MainButton.show();
+  }
 }
