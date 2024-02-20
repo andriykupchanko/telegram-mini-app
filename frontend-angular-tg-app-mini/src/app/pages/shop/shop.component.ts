230e3127cd6a13933881b1012b3e97e8bfba915b
Andriy Kupchanko <80577186+andriykupchanko@users.noreply.github.com>
2024-02-20 23:41:10 +0200

    create web blocks component product

--- frontend-angular-tg-app-mini/src/app/pages/shop/shop.component.ts
+++ frontend-angular-tg-app-mini/src/app/pages/shop/shop.component.ts
@@ -1,16 +1,35 @@
 import { Component, inject } from '@angular/core';
 import { TelegramService } from '../../service/telegram.service';
+import { ProductsService } from '../../service/products.service';
+import { ProductListComponent } from '../../components/product-list/product-list.component';
 
 @Component({
   selector: 'app-shop',
   standalone: true,
-  imports: [],
-  template: `<h1>main page</h1>`,
+  imports: [ProductListComponent],
+  template: `
+  <app-product-list 
+    title="Конкретна навичка"
+    subtitle="Вивчення використавуваних техсннологій , шоб розширити стек технологійних знань" 
+    [products]="products.byGroup['skill']"
+  />
+  <app-product-list 
+    title="Інтенсив "
+    subtitle="Експрес програма з практичними навичками" 
+    [products]="products.byGroup['intensive']"
+  />
+  <app-product-list 
+    title="Бесплатні курси "
+    subtitle=" Вивчення використавуваних технологій , шоб розширити стек технологійних знань" 
+    [products]="products.byGroup['course']"
+  />`,
 
 })
 export class ShopComponent {
   telegram = inject(TelegramService);
+  products = inject(ProductsService);
   constructor() {
     this.telegram.MainButton.show();
+    console.log(this.products.byGroup);
   }
 }
